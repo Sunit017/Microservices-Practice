e4fd9264c1d8afd86ca804021bff8f98fd54503b
Sunit017 <kumarsunit017@gmail.com>
2024-06-20 15:06:40 +0530

    Used WebClient

--- employee-service/src/main/java/com/example/employee_service/Service/EmployeeService.java
+++ employee-service/src/main/java/com/example/employee_service/Service/EmployeeService.java
@@ -3,7 +3,7 @@ package com.example.employee_service.Service;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.example.employee_service.Entity.Employee;
 import com.example.employee_service.Response.AddressResponse;
@@ -18,19 +18,33 @@ public class EmployeeService {
 	@Autowired
 	private ModelMapper modelMapper;
 	
+//	@Autowired
+//	private RestTemplate restTemplate;
+	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WebClient webClient;
 	
 	public EmployeeResponse getEmployeeById(int id)
 	{
 		Employee employee= employeeRepo.findById(id).get();
  		
 		EmployeeResponse employeeResponse= modelMapper.map(employee, EmployeeResponse.class);
-		AddressResponse addressResponse = restTemplate.getForObject("http://localhost:8091/address/{id}",AddressResponse.class, id);
+		AddressResponse addressResponse = webClient
+				.get()
+				.uri("/address/"+id)
+				.retrieve()
+				.bodyToMono(AddressResponse.class)
+				.block();
 		
 		employeeResponse.setAddressResponse(addressResponse);
 	
 		return employeeResponse;
+		
+	
 	}
+//	public AddressResponse callingThroughRestTemplate(int id)
+//	{
+//	return restTemplate.getForObject("http://localhost:8091/address/{id}",AddressResponse.class, id);
+//	}
 
 }
--- employee-service/src/main/java/com/example/employee_service/configuration/EmployeeConfiguration.java
+++ employee-service/src/main/java/com/example/employee_service/configuration/EmployeeConfiguration.java
@@ -1,13 +1,17 @@
 package com.example.employee_service.configuration;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class EmployeeConfiguration {
 
+	@Value("${addressService.base.url}")
+	private String addressBaseUrl;
 	@Bean
 	public ModelMapper modelMapper()
 	{ 
@@ -17,5 +21,14 @@ public class EmployeeConfiguration {
 	public RestTemplate restTemplate()
 	{
 		return new RestTemplate();
+	}
+	
+	@Bean
+	public WebClient client()
+	{
+		return WebClient
+				.builder()
+				.baseUrl(addressBaseUrl)
+				.build();
 	}
 }
