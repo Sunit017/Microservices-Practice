97d12f12122961cb186628779955a7fae1f3f681
sunitp <kumarsunit017@gmail.com>
2025-06-27 23:34:11 +0530

    apigateway added

---
+++ Online Insurance Platform/auth-service/src/main/java/com/practice/auth_service/AuthServiceApplication.java
@@ -0,0 +1,17 @@
+package com.practice.auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@Enable
+public class AuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+}
